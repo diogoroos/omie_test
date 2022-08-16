7bff0731ba4ea669b2c34a9c4c79aaf8b0c02e1a
Diogo Roos <diogoroos@gmail.com>
2022-08-16 19:47:15 -0300

    teste final

--- lib/main.dart
+++ lib/main.dart
@@ -42,48 +42,103 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String ua =
+      "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
+
   TextEditingController tec = TextEditingController();
 
-  Future<void> _incrementCounter() async {
-    await getHttp();
-    print('-------------------------------');
-    print('-------------------------------');
+  Future<void> _start() async {
+    await testeSimplesComDio();
+    print('-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x--x-x-x-x-x-x-x-x-x');
+    print('-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x');
+    //
+    // await testeSimples();
+    // print('-------------------------------');
+    // print('-------------------------------');
     //await ac.fazLogin('diogo.melo@omie.com.br', '753dRm12@', tec.text);
 
+    await testeCompleto();
+    print('-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x--x-x-x-x-x-x-x-x-x');
+    print('-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x--x-x-x-x-x-x-x-x-x');
+  }
+
+  Future<void> testeCompleto() async {
     SimpleDio.getInstance().instance.options.baseUrl = MyHomePage.appFlavor;
 
     AuthRepository ar = AuthRepository();
     AuthController ac = AuthController(ar);
-    print('MFA: ${tec.text}');
+    print('      >>>>>>>   TOKEN COMPLETO ---- MFA: ${tec.text}');
     var retTok =
         await ac.fazLogin(MyHomePage.email, MyHomePage.senha, tec.text);
     print('\nToken: $retTok');
 
     if (retTok != null && (retTok is Map) && retTok['status'] == 'OK') {
+      print('      >>>>>>>   ME COMPLETO ---- MFA: ${tec.text}');
       var retMe = await ac.buscaDadosUsuario();
       print('\nMe: $retMe');
 
       EmpresasRepository cr = EmpresasRepository();
       EmpresaController ec = EmpresaController(cr);
       try {
+        print('      >>>>>>>   APPS COMPLETO ---- MFA: ${tec.text}');
         var retApps = await ec.loadCompanies();
         print('\nApps: $retApps');
       } catch (e) {
         print(e);
       }
     } else {
-      print('-x-x-x-x-x-x-x-x-x-x-x-x-x-');
+      print('ERROR -x-x-x-x-x-x-x-x-x-x-x-x-x-');
       print(retTok.toString());
     }
   }
 
-  Future<void> getHttp() async {
-    const ua =
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
-
-    var request = Dio();
-
+  Future<void> testeSimplesComDio() async {
+    var dio = Dio();
     try {
+      dio.options.headers['User-Agent'] = ua;
+
+      print('      >>>>>>>   TOKEN DIO ---- MFA: ${tec.text}');
+      var tok = await dio.post(
+        '${MyHomePage.appFlavor}login/jwt/',
+        data: {
+          'email': MyHomePage.email,
+          'password': MyHomePage.senha,
+          'mfa': tec.text
+        },
+      );
+
+      print('\nToken:');
+      print(tok.data['token']);
+      await SharedPrefs().putString('token', tok.data['token']);
+      await SharedPrefs().putString('refresh_token', tok.data['refresh_token']);
+
+      dio.options.headers['authorization'] = tok.data['token'];
+
+      print('      >>>>>>>   ME COMPLETO');
+      var me = await dio.get('${MyHomePage.appFlavor}api/portal/users/me');
+      print('\nME:');
+      print(me);
+
+      print('      >>>>>>>   APPS COMPLETO');
+      var myApps =
+          await dio.get('${MyHomePage.appFlavor}api/portal/apps?app_type=OMIE');
+      print("\n myApps");
+      print(myApps);
+
+      print('      >>>>>>>   TOKEN APP');
+      var tokenBraziline = await dio.get(
+          '${MyHomePage.appFlavor}api/portal/apps/${myApps.data['appHash']}/token/');
+      print("\n tokenEmp");
+      print(tokenBraziline);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> testeSimples() async {
+    try {
+      SimpleDio.getInstance().instance.options.headers['User-Agent'] = ua;
+
       var tok = await SimpleDio.getInstance().instance.post(
         '${MyHomePage.appFlavor}login/jwt/',
         data: {
@@ -98,9 +153,10 @@ class _MyHomePageState extends State<MyHomePage> {
       await SharedPrefs().putString('token', tok.data['token']);
       await SharedPrefs().putString('refresh_token', tok.data['refresh_token']);
 
-      request.options.headers["Authorization"] = "Bearer ${tok.data['token']}";
-      request.options.baseUrl = MyHomePage.appFlavor;
+      SimpleDio.getInstance().instance.options.headers['authorization'] =
+          tok.data['token'];
 
+      print('      >>>>>>>   ME SIMPLES');
       var me = await SimpleDio.getInstance()
           .instance
           .get('${MyHomePage.appFlavor}api/portal/users/me');
@@ -108,16 +164,16 @@ class _MyHomePageState extends State<MyHomePage> {
       print('\nME:');
       print(me);
 
+      print('      >>>>>>>   APPS SIMPLES');
       var myApps = await SimpleDio.getInstance()
           .instance
           .get('${MyHomePage.appFlavor}api/portal/apps?app_type=OMIE');
-
       print("\n myApps");
       print(myApps);
 
+      print('      >>>>>>>   TOKEN APP SIMPLES');
       var tokenBraziline = await SimpleDio.getInstance().instance.get(
           '${MyHomePage.appFlavor}api/portal/apps/${myApps.data['appHash']}/token/');
-
       print("\n tokenEmp");
       print(tokenBraziline);
     } catch (e) {
@@ -144,7 +200,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          _incrementCounter();
+          await _start();
         },
         child: const Icon(Icons.add),
       ),
